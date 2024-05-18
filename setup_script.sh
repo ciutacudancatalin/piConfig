1b7a80f9fd317973bad2f57bd053d71160e43e82
Ciutacu Dan-Catalin <55996621+ciutacudancatalin@users.noreply.github.com>
2024-05-18 12:32:21 +0300

    Update setup_script.sh
    updated wrong file....

--- setup_script.sh
+++ setup_script.sh
@@ -1,32 +1,49 @@
-[cm4]
-otg_mode=1
+#update and upgrade
+sudo apt-get update -y ; sudo apt-get upgrade -y;
 
-[pi4]
-hdmi_force_hotplug:0=1
-hdmi_force_hotplug:1=1
+#create folder for airplay in /home/user and navigate to it
+mkdir airplay;
+cd airplay;
 
-[all]
-disable_splash=1
-disable_overscan=1
-hdmi_drive=2
-hdmi_blanking=1
-hdmi_force_edid_audio=1
-hdmi_force_hotplug=1
-hdmi_group=0
-dtparam=i2c_arm=on
-dtparam=i2s=on
-dtparam=audio=on
-#dtoverlay=disable-wifi
-#commented by me idtoverlay=disable-bt
+#install git
+sudo apt-get install git -y;
 
-#changes
-#overclock
-over_voltage=6
-arm_freq=2000
-gpu_freq=75
+#copy project from git
+git clone https://github.com/mikebrady/shairport-sync.git; 
 
-#audio lag
-disable_audio_dither=on
+#navigate to git repo
+cd shairport-sync;
 
-# Disable wifi power management
-wireless-power off
+#setup airplay using shairplay
+sudo apt install autoconf libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev -y ; sudo apt install avahi-daemon libavahi-client-dev -y ; sudo apt install libssl-dev -y;  autoreconf -i -f ; ./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata; make;sudo make install
+
+#enable airplay service
+sudo systemctl enable shairport-sync;
+
+#navigate to home
+cd /home/catalin;
+
+#create folder for cooler and navigate to it
+mkdir cooler;
+cd cooler;
+
+#copy cooler(pimoroni-fanshim) repo and navigate to it
+git clone https://github.com/pimoroni/fanshim-python;
+cd fanshim-python;
+
+#install cooler software
+sudo apt-get install python3-pip;
+sudo ./install.sh;
+cd examples;
+sudo ./install-service.sh --off-threshold 55 --on-threshold 75 --low-temp 55 --high-temp 75 --preempt --noled --nobutton;
+sudo systemctl enable pimoroni-fanshim.service;
+
+#navigate to /home/user
+cd /home/catalin;
+
+#create folder for pi-hole
+mkdir pihole;
+cd pihole;
+
+#download pi-hole (human interaction needed for cofiguration)
+curl -sSL https://install.pi-hole.net | bash;
